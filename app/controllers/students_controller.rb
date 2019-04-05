618512bfc54b81c2649893ea6ac6cfa251c9772b
Caridad Rivera <ccrive10@gmail.com>
2019-04-05 11:33:14 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,24 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
+
+  def activate
+    @student = Student.find(params[:id])    #I want to find my student instance
+    @student.active = !@student.active  #make their default status change to true
+    @student.save #save it
+    redirect_to student_path(@student)
+
+  end
+
+
+
 
   private
 
